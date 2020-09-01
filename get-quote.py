f6a5961711c46a7cb3182d874d8220b279e5fee3
Aditya <aiyer786@gmail.com>
2020-09-01 15:31:59 +0530

    Performed the next steps

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,18 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
+  f = open("python-random-quote\quotes.txt","a")
+  f.write("\nwillem DAFOE!")
+  f.close()
   f = open("python-random-quote\quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+  last = 20
+  rnd = random.randint(0, last)
+  print(quotes[rnd],end='')
   rnd = random.randint(0, last)
   print(quotes[rnd])
+
 
 if __name__== "__main__":
   primary()
